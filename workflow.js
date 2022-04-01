366193988e46d7c7f6ac4983ebf8c99d9c711236
Green_Lab <greencoder001@outlook.com>
2022-04-01 21:30:33 +0200

    Fixed an error

--- workflow.js
+++ workflow.js
@@ -1,6 +1,6 @@
 const { check } = require('./main.js')
 const cwd = process.cwd()
 const path = require('path')
-const licenseComplainsRC = require(path.join(process.cwd(), '.licensecomplainsrc'))
+const licenseComplainsRC = JSON.parse(fs.readFileSync(path.join(process.cwd(), '.licensecomplainsrc')).toString('utf-8'))
 
 check(licenseComplainsRC, true, true, cwd)
