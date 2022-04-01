b3e1908637d075af337f81dfb7f7cdcd1830961d
Green_Lab <greencoder001@outlook.com>
2022-04-01 21:34:53 +0200

    Updated workflow.js

--- workflow.js
+++ workflow.js
@@ -1,6 +1,7 @@
 const { check } = require('./main.js')
 const cwd = process.cwd()
 const path = require('path')
+const fs = require('fs')
 const licenseComplainsRC = JSON.parse(fs.readFileSync(path.join(process.cwd(), '.licensecomplainsrc')).toString('utf-8'))
 
 check(licenseComplainsRC, true, true, cwd)
