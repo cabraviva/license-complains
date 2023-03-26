59f1c1b422da166e216b956a2faaf27b192ada61
Green_Lab <greencoder001@outlook.com>
2023-03-26 14:04:32 +0200

    Added Python 2.0

--- example.js
+++ example.js
@@ -5,8 +5,8 @@ Complains.check({
     modifications: true, // True means you want to modify the code of a package
     distribution: true, // True means that you want to distribute the code (Needs to be true if you want to distribute your code)
     privateUse: true, // True means you want to use the package privately
-    warranty: true, // True means if you are ok with the fact that the dependency doesn't give you warranty
-    liability: true, // True means if you are ok with the fact that the dependency doesn't give you liability
+    warranty: true, // True means you are ok with the fact that the dependency doesn't give you warranty
+    liability: true, // True means you are ok with the fact that the dependency doesn't give you liability
     copyRightNotice: true, // True means you are ok with including copyright notices
     licenseNotice: true, // True means you are ok with including the original LICENSE file of the package
     patentUse: true, // True means that you want to use the package for patent purposes
